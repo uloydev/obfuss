5c2be8eace6faa81fdf32a67c3a2bc8686e00fb3
fathrahh <gazoon3@gmail.com>
2024-02-01 21:48:32 +0700

    fix: fk trans absen mahasiswa

--- entities/trans_absen_mahasiswa.go
+++ entities/trans_absen_mahasiswa.go
@@ -10,9 +10,9 @@ type TransAbsenMahasiswa struct {
 	gorm.Model
 	ID              int `gorm:"type:int(11);auto_increment;"`
 	IDPertemuan     int
-	JadwalPertemuan TransJadwalPertemuan
+	JadwalPertemuan TransJadwalPertemuan `gorm:"foreignKey:IDPertemuan"`
 	IDMahasiswa     int
-	Mahasiswa       Mahasiswa
+	Mahasiswa       Mahasiswa `gorm:"foreignKey:IDMahasiswa"`
 	IsHadir         string    `gorm:"type:enum('yes','no');"`
 	AbsenDate       time.Time `gorm:"type:datetime;"`
 	Keterangan      string    `gorm:"type:enum('daring','luring');"`
