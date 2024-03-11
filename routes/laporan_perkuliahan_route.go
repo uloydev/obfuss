83f385aeefa6e1f37c1593446c2c7a5efa67633f
Muhammad Fathurrahman <gazoon3@gmail.com>
2024-03-11 17:09:01 +0700

    feat: delete laporan perkuliahan

--- routes/laporan_perkuliahan_route.go
+++ routes/laporan_perkuliahan_route.go
@@ -24,4 +24,5 @@ func (r *LaporanPerkuliahanRoute) Register(app *gin.RouterGroup) {
 	api := app.Group(r.prefix, middlewares.RequestId(), middlewares.Authorization())
 	api.GET("/", r.handler.GetAll)
 	api.POST("/save-trans", r.handler.SaveTransLaporanPerkuliahan)
+	api.DELETE("/:id-pertemuan/pertemuan", r.handler.Delete)
 }
